462ccdbf69e8490cd83d9083c09bb31be23658f1
byfranfx <71705230+byfranfx@users.noreply.github.com>
2021-01-13 20:36:47 +0100

    ...

--- app/src/main/java/com/example/t3_activitat1/MainActivity.java
+++ app/src/main/java/com/example/t3_activitat1/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -31,24 +32,30 @@ public class MainActivity extends AppCompatActivity {
         gameListNotDone.add(new Games(5, "i5","Team Fortress 2","FPS, Disparos de heroe, Multijugador","Free To Play",false));
         gameListNotDone.add(new Games(6, "i6","Brawlhalla","Multijugador, Lucha,  2D","Free To Play",false));
 
+        /*new Games(1, "i1","Counter-Strike: GLobal Offensive","FPS, Disparos, Multijugador, Competitivo","Free To Play", true);
+        new Games(2, "i2","Dota 2","FPS, Multijugador, Estrategia","Free To Play",false);
+        new Games(3, "i3","Apex Legends™","FPS, Primera persona, Disparos","Free To Play",false);
+        new Games(4, "i4","Deastiny 2","JcJ (PvP), JcE (PvE), Diparos","Free To Play",false);
+        new Games(5, "i5","Team Fortress 2","FPS, Disparos de heroe, Multijugador","Free To Play",false);
+        new Games(6, "i6","Brawlhalla","Multijugador, Lucha,  2D","Free To Play",false);
+
+        Games g = new Games();
+
+        if(g.isCheckbox()){
+
+        }*/
 
 
         listAdapter0 = new GameAdapter(this, R.layout.list_games, gameListNotDone);
-        ListView mainList = findViewById(R.id.mainListViewNotDone);
+        ListView mainList = (ListView) findViewById(R.id.mainListViewNotDone);
         mainList.setAdapter(listAdapter0);
 
         listAdapter1 = new GameAdapter(this, R.layout.list_games, gameListDone);
-        ListView mainListDone = findViewById(R.id.mainListViewDone);
+        ListView mainListDone = (ListView) findViewById(R.id.mainListViewDone);
         mainListDone.setAdapter(listAdapter1);
 
-        mainList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(view.getContext(), DetallGame.class);
-                i.putExtra("mylist", gameListDone.get(position));
-                startActivity(i);
-            }
-        });
+        // setOnCLickListener
+
     }
 
     public void checkboxUpdate(Games g){
@@ -67,21 +74,6 @@ public class MainActivity extends AppCompatActivity {
         }
         listAdapter0.notifyDataSetChanged();
         listAdapter1.notifyDataSetChanged();
-
-        /*if(g.isCheckbox()){
-            gameListDone.remove(g);
-            gameListNotDone.add(g);
-            Toast toast1 = Toast.makeText(getApplicationContext(), "marcat", Toast.LENGTH_LONG);
-            toast1.show();
-            g.setCheckbox(false);
-        }else{
-            gameListNotDone.remove(g);
-            gameListDone.add(g);
-            Toast toast1 = Toast.makeText(getApplicationContext(), "desmarcat", Toast.LENGTH_LONG);
-            toast1.show();
-            g.setCheckbox(true);
-        }*/
-        //listAdapter.notifyDataSetChanged();
     }
 
 }
