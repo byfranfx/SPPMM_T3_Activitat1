88a64f96a7e5df331aa63a7f2460135c16c57932
byfranfx <71705230+byfranfx@users.noreply.github.com>
2021-01-19 18:31:09 +0100

    ...

--- app/src/main/java/com/example/t3_activitat1/AfegirGame.java
+++ app/src/main/java/com/example/t3_activitat1/AfegirGame.java
@@ -4,12 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.Iterator;
@@ -17,34 +14,44 @@ import java.util.Iterator;
 public class AfegirGame extends AppCompatActivity {
 
     private static final int CODI_PETICIO = 0;
-    private static final int PICK_IMAGE = 88;
 
     public static final String keyName = "keyName";
     public static final String keyDesc = "keyDesc";
     public static final String keyType = "keyType";
 
-    ImageView foto_gallery;
-    Uri imageUri;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.afegir_game);
-
-        foto_gallery = (ImageView)findViewById(R.id.foto_gallery);
-
-        foto_gallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openGallery();
-            }
-        });
     }
 
-    private void openGallery(){
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(gallery, PICK_IMAGE);
+
+    /*@Override
+    public void finish() {
+
+        EditText text = findViewById(R.id.nameEditText);
+        String valor = text.getText().toString();
+        Intent data = new Intent();
+        data.putExtra(keyName, valor);
+        setResult(RESULT_OK, data);
+
+        EditText desc = findViewById(R.id.descEditText);
+        valor = desc.getText().toString();
+        data = new Intent();
+        data.putExtra(keyDesc, valor);
+        setResult(RESULT_OK, data);
+
+        EditText type = findViewById(R.id.typeEditText);
+        valor = type.getText().toString();
+        data = new Intent();
+        data.putExtra(keyType, valor);
+        setResult(RESULT_OK, data);
+        super.finish();
     }
+
+    public void tancaActivitat(View view) {
+        finish();
+    }*/
 
     public void enviarDadesOnClick(View view) {
 
@@ -59,7 +66,7 @@ public class AfegirGame extends AppCompatActivity {
         EditText et3 = (EditText) findViewById(R.id.typeEditText);
         String typeEditText = et3.getText().toString();
 
-        ImageView et4 = (ImageView) findViewById(R.id.foto_gallery);
+
 
         if (nameEditText.matches("") || descEditText.matches("") || typeEditText.matches("")){
 
@@ -83,61 +90,17 @@ public class AfegirGame extends AppCompatActivity {
             System.out.println("El número de elementos es de " + lengthList);
 
             lengthList++;
-            MainActivity.gameListNotDone.add(new Games(lengthList, imageUri.toString(), nameEditText, descEditText, typeEditText, false));
-            System.out.println("El número de elementos ahoa es de " + lengthList);
+            MainActivity.gameListNotDone.add(new Games(lengthList, "", nameEditText, descEditText, typeEditText, false));
+            System.out.println("El número de elementos ahora es de " + lengthList);
 
-            Context context = getApplicationContext();
+            /*Context context = getApplicationContext();
             String text = MainActivity.gameListNotDone.get(lengthList).getName();
             //System.out.println (car.get(i).getPrice());
             int duration = Toast.LENGTH_SHORT;
-            Toast.makeText(context, text, duration).show();
+            Toast.makeText(context, text, duration).show();*/
 
             clearOnClick(view);
         }
-
-
-        /*if (nameEditText.matches("") || descEditText.matches("") || typeEditText.matches("")){
-
-            Toast.makeText(this, "You did not enter a game name", Toast.LENGTH_SHORT).show();
-            return;
-
-        } else {
-            int lengthList =  MainActivity.gameListNotDone.size();
-            System.out.println("El número de elementos es de ");
-
-            lengthList++;
-            MainActivity.gameListNotDone.add(new Games(lengthList, "",nameEditText,descEditText,typeEditText, false));
-            System.out.println(lengthList);
-
-
-            Context context = getApplicationContext();
-            String text = Integer.toString(lengthList);
-            int duration = Toast.LENGTH_SHORT;
-            Toast.makeText(context, text, duration).show();
-
-            clearOnClick(view);
-        }*/
-
-
-
-
-
-        //agafar el text introduït
-        /*EditText et1 = (EditText) findViewById(R.id.nameEditText);
-        String nameEditText = et1.getText().toString();
-
-        EditText et2 = (EditText) findViewById(R.id.descEditText);
-        String descEditText = et2.getText().toString();
-
-        EditText et3 = (EditText) findViewById(R.id.typeEditText);
-        String typeEditText = et3.getText().toString();
-
-        //Accions a fer...
-        Intent i = new Intent(this, MainActivity.class);
-        i.putExtra(keyName,nameEditText);
-        i.putExtra(keyDesc,descEditText);
-        i.putExtra(keyType,typeEditText);
-        startActivityForResult(i,CODI_PETICIO);*/
     }
 
     public void cancelOnClick(View view)
